bb331b5648beea2eff8293f79cf470af511fe305
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-18 15:38:27 +0600

    Final Schedule test with rabbitmq

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from time import sleep
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
+import json
 
 @shared_task
 def sub(x, y):
@@ -18,15 +19,24 @@ def clear_redis_data(key):
     print(f'Redis Data Cleard: {key}')
     return key
 
+@shared_task
+def clear_rabbitmq_data(key):
+    print(f'rabbitmq Data Cleard: {key}')
+    return key
+
+
 # Programitically define interval. We do same thing using django admin interval
 # 1. Define the interval (e.g., every 10 seconds)
 schedule, created = IntervalSchedule.objects.get_or_create(
-    every=10,
+    every=4,
     period=IntervalSchedule.SECONDS,
 )
 # 2. Create the periodic task
-PeriodicTask.objects.create(
-    interval=schedule,
+PeriodicTask.objects.get_or_create(
     name='Importing data every 10s',
-    task='myapp.tasks.clear_redis_data',
+    defaults={
+        'task': 'myapp.tasks.clear_rabbitmq_data',
+        'interval': schedule,
+        'args': json.dumps(["hello rabbit"]),
+    }
 )
